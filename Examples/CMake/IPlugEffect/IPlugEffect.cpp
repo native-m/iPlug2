20d1c48dd0aae764af0e62af6c0cb347deb6f83f
Muhammad Gusti Nurfathin (Native-M) <gustipart2@gmail.com>
2023-07-21 22:56:18 +0700

    Add CMakeLists.txt files

---
+++ Examples/CMake/IPlugEffect/IPlugEffect.cpp
@@ -0,0 +1,39 @@
+#include "IPlugEffect.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControls.h"
+
+IPlugEffect::IPlugEffect(const InstanceInfo& info)
+  : Plugin(info, MakeConfig(kNumParams, kNumPresets))
+{
+  GetParam(kGain)->InitDouble("Gain", 0., 0., 100.0, 0.01, "%");
+
+  mMakeGraphicsFunc = [&]() {
+    return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, GetScaleForScreen(PLUG_WIDTH, PLUG_HEIGHT));
+  };
+
+  mLayoutFunc = [&](IGraphics* pGraphics) {
+    pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
+    pGraphics->AttachPanelBackground(COLOR_GRAY);
+    pGraphics->LoadFont("Roboto-Regular", IPLUG_EXAMPLE_RESOURCE_PATH"/Roboto-Regular.ttf");
+    const IRECT b = pGraphics->GetBounds();
+    pGraphics->AttachControl(new ITextControl(b.GetMidVPadded(50), "Hello iPlug 2!", IText(50)));
+    pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(-100), kGain));
+  };
+}
+
+void IPlugEffect::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+{
+  const double gain = GetParam(kGain)->Value() / 100.;
+  const int nChans = NOutChansConnected();
+
+  for (int s = 0; s < nFrames; s++) {
+    for (int c = 0; c < nChans; c++) {
+      outputs[c][s] = inputs[c][s] * gain;
+    }
+  }
+}
+
+Plugin* IPlugCreateInstance(const iplug::InstanceInfo& instance_info)
+{
+  return new IPlugEffect(instance_info);
+}
---
+++ Examples/CMake/IPlugEffect/IPlugEffect.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "IPlug_include_in_plug_hdr.h"
+
+const int kNumPresets = 1;
+
+enum EParams
+{
+  kGain = 0,
+  kNumParams
+};
+
+using namespace iplug;
+using namespace igraphics;
+
+class IPlugEffect final : public Plugin
+{
+public:
+  IPlugEffect(const InstanceInfo& info);
+  void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+};
